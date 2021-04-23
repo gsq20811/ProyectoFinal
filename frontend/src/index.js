9e5ddccaa0125005eb539cb7460dac190e627aa2
Guillermo Savero <gsq2081@gmail.com>
2021-04-22 23:32:46 -0500

    cambio2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${process.cwd()}/dist`));
 
 app.get('*', (req, res) => {
-    return res.sendFile(path.join(`${__dirname}/dist/index.html`));
+    return res.sendFile(path.join(`${process.cwd()}/dist/index.html`));
 });
 
 app.listen(process.env.PORT || 4400);
